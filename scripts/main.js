eec589352a6ebd498151f1c00d3b8ef3407a357e
Vaughn Nedderman <vaughn@nedderman.com>
2023-05-01 13:42:54 -0400

    index.html

--- scripts/main.js
+++ scripts/main.js
@@ -4,46 +4,42 @@ let hungerLevel = 50;
 let mood = "Sad";
 
 // Get DOM elements for the pet's status
-const petNameEl = document.querySelector("pet-name");
-const hungerEl = document.querySelector("hunger-level");
-const moodEl = document.querySelector("mood");
+const petNameEl = document.querySelector("#pet-name");
+const hungerEl = document.querySelector("#hunger-level");
+const moodEl = document.querySelector("#mood");
 
 //Get DOM elements for the action buttons
 const feedButton = document.querySelector("#feed-button");
 const playButton = document.querySelector("#play-button");
 
 function playPet() {
+  hungerLevel -= 10;
   if (hungerLevel > 30) {
-    hungerLevel -= 10;
     hungerEl.innerText = hungerLevel + "%";
     mood = "Happy";
     moodEl.innerText = mood;
-  } else if (hungerLevel < 30 && hungerLevel > 10) {
-    hungerLevel -= 10;
+  } else if (hungerLevel <= 30 && hungerLevel > 10) {
     hungerEl.innerText = hungerLevel + "%";
     mood = "Hangry";
     moodEl.innerText = mood;
-  } else {
-    hungerLevel = 0;
-    hungerEl.innerText = 0 + "%";
+  } else if (hungerLevel <= 10 && hungerLevel >= 0) {
+    hungerEl.innerText = hungerLevel + "%";
     mood = "Starving";
     moodEl.innerText = mood;
   }
 }
 
 function feedPet() {
-  if (hungerLevel < 10) {
-    hungerLevel += 10;
+  hungerLevel += 10;
+  if (hungerLevel <= 10 && hungerLevel >= 0) {
     hungerEl.innerText = hungerLevel + "%";
     mood = "Starving";
     moodEl.innerText = mood;
-  } else if (hungerLevel > 10 && hungerLevel < 30) {
-    hungerLevel += 10;
+  } else if (hungerLevel > 10 && hungerLevel <= 30) {
     hungerEl.innerText = hungerLevel + "%";
     mood = "Hangry";
     moodEl.innerText = mood;
-  } else if (hungerLevel < 100 && hungerLevel > 30) {
-    hungerLevel += 10;
+  } else if (hungerLevel <= 100 && hungerLevel > 30) {
     hungerEl.innerText = hungerLevel + "%";
     mood = "Happy";
     moodEl.innerText = mood;

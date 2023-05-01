a8d37ddef5103011d39902c403766cb93ac49554
Vaughn Nedderman <vaughn@nedderman.com>
2023-05-01 12:47:37 -0400

    style changes for css

--- scripts/main.js
+++ scripts/main.js
@@ -12,18 +12,43 @@ const moodEl = document.querySelector("mood");
 const feedButton = document.querySelector("#feed-button");
 const playButton = document.querySelector("#play-button");
 
-function feedPet() {
-  if (hungerLevel < 100) {
-    hungerLevel += 10;
+function playPet() {
+  if (hungerLevel > 30) {
+    hungerLevel -= 10;
     hungerEl.innerText = hungerLevel + "%";
-    mood = "Sad";
+    mood = "Happy";
+    moodEl.innerText = mood;
+  } else if (hungerLevel < 30 && hungerLevel > 10) {
+    hungerLevel -= 10;
+    hungerEl.innerText = hungerLevel + "%";
+    mood = "Hangry";
     moodEl.innerText = mood;
   } else {
     hungerLevel = 0;
     hungerEl.innerText = 0 + "%";
+    mood = "Starving";
+    moodEl.innerText = mood;
+  }
+}
+
+function feedPet() {
+  if (hungerLevel < 10) {
+    hungerLevel += 10;
+    hungerEl.innerText = hungerLevel + "%";
+    mood = "Starving";
+    moodEl.innerText = mood;
+  } else if (hungerLevel > 10 && hungerLevel < 30) {
+    hungerLevel += 10;
+    hungerEl.innerText = hungerLevel + "%";
+    mood = "Hangry";
+    moodEl.innerText = mood;
+  } else if (hungerLevel < 100 && hungerLevel > 30) {
+    hungerLevel += 10;
+    hungerEl.innerText = hungerLevel + "%";
     mood = "Happy";
     moodEl.innerText = mood;
   }
 }
 
 feedButton.addEventListener("click", feedPet);
+playButton.addEventListener("click", playPet);
